137eea279af5fc60290ae938c37661dc207d8e5a
Valery Nevzorov <54248784+nevzorov46@users.noreply.github.com>
2021-01-02 10:18:35 +0300

    Initial Commit

---
+++ The Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Light
+//
+//  Created by Иван Карамазов on 02.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
